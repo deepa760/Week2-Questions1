c0b3dfcd62afa713e75864eb22522cf907921ccc
Hema123 <hemaghg@gmail.com>
2020-06-17 15:55:54 +0200

    Initial commit

---
+++ src/Question1.java
@@ -0,0 +1,13 @@
+public class Question1 {
+    public static void main(String args[])
+    {
+        int[] a= new int[] {34, 4, 12, 65};
+
+        //for(int i=0; i<a.length; i++)
+            System.out.println(a[0]);
+            System.out.println(a[1]);
+            System.out.println(a[2]);
+             System.out.println(a[3]);
+
+    }
+}
